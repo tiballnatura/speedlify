a829fd05f4ede5af87d00c8b2894e3633d75873c
Timo Betz <timo.betz@allnatura.de>
2026-07-02 08:53:42 +0200

    Update allnatura.de configuration: increase runs to 3 and add additional URLs for testing

--- _data/sites/allnatura_de.js
+++ _data/sites/allnatura_de.js
@@ -3,11 +3,14 @@ module.exports = {
 	description: "Testing allnatura.de",
 	skip: false,
 	options: {
-		runs: 1,
+		runs: 3,
 		frequency: 1, // (in minutes)
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.allnatura.de/"
+		"https://www.allnatura.de/",
+		"https://www.allnatura.de/schlafen/matratzen/naturlatex-matratzen",
+		"https://www.allnatura.de/p/naturlatex-matratze-supra-comfort-89",
+		"https://www.allnatura.de/ratgeber",
 	]
 };
